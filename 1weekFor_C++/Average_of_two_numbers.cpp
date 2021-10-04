a30bbd34a3361542fea677d1c9a37ccf5a3ee43b
Fariya Islam <fariyaislam2096@gmail.com>
2021-10-04 22:56:30 +0600

    Average numbers

---
+++ 1weekFor_C++/Average_of_two_numbers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float num1, num2, sum, avg;
+    cout << "enter two numbers:";
+    cin >> num1 >> num2;
+    sum = num1 + num2;
+    avg = sum/2;
+    cout << "sum =" << sum << endl;
+    cout << "average = " << avg << endl;
+
+
+         return 0;
+}
