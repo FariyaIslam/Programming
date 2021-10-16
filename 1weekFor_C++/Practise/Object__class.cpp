f11cd8ed397824de4cb71d0de26ce754179f3165
Fariya Islam <fariyaislam2096@gmail.com>
2021-10-16 22:50:14 +0600

    Radius circle

--- 1weekFor_C++/Practise/Object__class.cpp
+++ 1weekFor_C++/Practise/Object__class.cpp
@@ -1,1 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+class circle
+{
+    float radius;
+
+public:
+    void calculateArea(float);
+    void displayArea();
+};
+
+void circle ::calculateArea(float random)
+{
+    radius = random;
+}
+void circle ::displayArea()
+{
+    cout << "radius is: " << radius <<endl;
+}
+
+int main()
+{
+    circle abs;
+    abs.calculateArea(56.99);
+    abs.displayArea();
+    return 0;
+}
